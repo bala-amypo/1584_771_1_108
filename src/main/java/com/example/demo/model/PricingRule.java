afcee120a6bd630ecd4131054ace99bfe8db87e1
bala-amypo <bala@amypo.in>
2025-12-15 10:40:28 +0000

    User commit

--- src/main/java/com/example/demo/model/PricingRule.java
+++ src/main/java/com/example/demo/model/PricingRule.java
@@ -6,5 +6,84 @@ public class PricingRule {
     private String description;
     private Integer minRemainingSeats;
     private Integer maxRemainingSeats;
-    private Integer 
+    private Integer daysBeforeEvent;
+    private Double priceMultiplier;
+    private Boolean active;
+    
+    public PricingRule(String ruleCode, String description, Integer minRemainingSeats, Integer maxRemainingSeats,
+            Integer daysBeforeEvent, Double priceMultiplier, Boolean active) {
+        this.ruleCode = ruleCode;
+        this.description = description;
+        this.minRemainingSeats = minRemainingSeats;
+        this.maxRemainingSeats = maxRemainingSeats;
+        this.daysBeforeEvent = daysBeforeEvent;
+        this.priceMultiplier = priceMultiplier;
+        this.active = active;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMinRemainingSeats(Integer minRemainingSeats) {
+        this.minRemainingSeats = minRemainingSeats;
+    }
+
+    public void setMaxRemainingSeats(Integer maxRemainingSeats) {
+        this.maxRemainingSeats = maxRemainingSeats;
+    }
+
+    public void setDaysBeforeEvent(Integer daysBeforeEvent) {
+        this.daysBeforeEvent = daysBeforeEvent;
+    }
+
+    public void setPriceMultiplier(Double priceMultiplier) {
+        this.priceMultiplier = priceMultiplier;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getRuleCode() {
+        return ruleCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getMinRemainingSeats() {
+        return minRemainingSeats;
+    }
+
+    public Integer getMaxRemainingSeats() {
+        return maxRemainingSeats;
+    }
+
+    public Integer getDaysBeforeEvent() {
+        return daysBeforeEvent;
+    }
+
+    public Double getPriceMultiplier() {
+        return priceMultiplier;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    
 }
