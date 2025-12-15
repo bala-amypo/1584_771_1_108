b612723b628bc6f5de20b375b74fe3844f424129
bala-amypo <bala@amypo.in>
2025-12-15 10:35:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/PricingRule.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class PricingRule {
+    private long id;
+    private String ruleCode;
+    private String description;
+    private Integer minRemainingSeats;
+    private Integer maxRemainingSeats;
+    private Integer 
+}
--- src/main/java/com/example/demo/model/SeatInventoryRecord.java
+++ src/main/java/com/example/demo/model/SeatInventoryRecord.java
@@ -1,8 +1,59 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class SeatInventoryRecord {
     private long id;
     private long eventId;
     private Integer totalSeats;
-    private Integer remainingSeats
+    private Integer remainingSeats;
+    private LocalDateTime updatedAt;
+
+    public SeatInventoryRecord(long eventId, Integer totalSeats, Integer remainingSeats, LocalDateTime updatedAt) {
+        this.eventId = eventId;
+        this.totalSeats = totalSeats;
+        this.remainingSeats = remainingSeats;
+        this.updatedAt = updatedAt;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setTotalSeats(Integer totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public void setRemainingSeats(Integer remainingSeats) {
+        this.remainingSeats = remainingSeats;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public Integer getTotalSeats() {
+        return totalSeats;
+    }
+
+    public Integer getRemainingSeats() {
+        return remainingSeats;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
 }
