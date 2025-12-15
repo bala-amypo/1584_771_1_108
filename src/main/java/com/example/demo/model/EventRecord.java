54d6290f43e44f101ccccf39fdf185cbd79f44a7
bala-amypo <bala@amypo.in>
2025-12-15 10:30:50 +0000

    User commit

--- src/main/java/com/example/demo/model/EventRecord.java
+++ src/main/java/com/example/demo/model/EventRecord.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class EventRecord {
     private long id;
@@ -11,5 +12,84 @@ public class EventRecord {
     private Double basePrice;
     private LocalDateTime createdAt;
     private Boolean active;
+    
+    public EventRecord(String eventCode, String eventName, String venue, LocalDate eventDate, Double basePrice,
+            LocalDateTime createdAt, Boolean active) {
+        this.eventCode = eventCode;
+        this.eventName = eventName;
+        this.venue = venue;
+        this.eventDate = eventDate;
+        this.basePrice = basePrice;
+        this.createdAt = createdAt;
+        this.active = active;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEventCode(String eventCode) {
+        this.eventCode = eventCode;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setBasePrice(Double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEventCode() {
+        return eventCode;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    
+
+
+    
 }
---
+++ src/main/java/com/example/demo/model/SeatInventoryRecord.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public class SeatInventoryRecord {
+    private long id;
+    private long eventId;
+    private Integer totalSeats;
+    private Integer remainingSeats
+}
